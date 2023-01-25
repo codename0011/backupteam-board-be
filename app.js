0c5f8975a86b672b96e713a40e6c823c42b3b61d
DESKTOP-5KAU2E0\jds75 <jds7589@gmail.com>
2023-01-25 19:03:37 +0900

    페이지네이션(90% 구현)

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require ('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(cors())
 
 const movies = [
    {id: 1, movie_title: "Misérables, Les", hit_count: 23, user_id: 1, created_at: "2022-08-11 00:40:32"},
@@ -41,46 +42,77 @@ const users = [
    {id: 10,name: "Kaylee Jakoubec", email: "kjakoubec2i@epa.gov"}
 ]
 
-//영화 제목 리스트
+//영화 제목 리스트 (페이지네이션)
 app.get('/movies', (req, res) => {
-  res.send (movies.map(movie =>({
+   const page  = req.query.page || 1
+   const copymovies = [...movies]
+   const endpage = Math.ceil(movies.length / 3)
+   const startIndex = (page - 1) * 3
+   const pagimovies = copymovies.splice(startIndex, 3)
+   
+  const movielist = pagimovies.map(movie =>({
    ...movie,
    name:users.find(user => user.id === movie.user_id).name
-  })))
-})
+  }))
 
+  movielist.sort((a, b) => {
+   const preTime = new Date(a.created_at).getTime()
+   const curTime = new Date(b.created_at).getTime()
+   return curTime - preTime
+  })
+
+  console.log("startIndex : ", startIndex)
+  console.log("endpage : ",  endpage)
+
+  
+  console.log ("endpage : ", endpage)
+   
+  res.send({
+      pagedeteil :{
+          endpage
+      },
+      movies :pagimovies
+   })
+})
 
 //hit_count 증가
-app.get('/moives/:id',(req,res) =>{
+app.get('/movies/:id',(req,res) =>{
 //1.사용자가 보낸 id를 가져옴
- const id  = req.params.id
- //2.id에 해당한 movie를 가져옴
- const movieid = movies.filter(movie =>movie.id === Number(id))
- //3.가져온 movie 에서 조회수(hit_count) + 1을 더한 객체 만듬
- const hit = movieid.find(movieid => movieid.hit_count++)
- //4.hit_count + 1 더한 객체를 movie내에서 기존 객체를 치환 splice, findIndex(힌트)
- const findmovie = movies.findIndex(movies => movies.hit_count === movieid.hit_count)
- //5.hit_count + 1 더한 객체를 반환
- if(findIndex > -1){
-   movies.splice(findmovie, 1,hit)
- }
-  res.send(movieid)
+const { id } = req.params
+//2.id에 해당한 movie를 가져옴
+const moviefind = movies.find((movie) => movie.id === Number(id))         //Number?? (숙제!)
+//3.가져온 movie 에서 hit_count + 1 을 한 객체만듬
+const findmovie = {...moviefind, hit_count: moviefind.hit_count + 1}       //...?(구글링이 안돼면 팀원분들에게 도!!움!!) 문법 (숙제!)
+//4.hit_count + 1 한 객체를 movie 내에 기존 객체에 치환(findIndex , splice 사용)
+const countPush = movies.findIndex((movie) => movie.id === Number(id))
+movies.splice(countPush, 1, findmovie) //splice? (배열로 부터 특정 범위를 삭제 및 새로운 값 추가 및 기존 값 대체 가능)
+       //구문 array.splice(startNum, deleteCount??, isertValue)
+       //startNum(시작 순서) 부터, deleteCount(제거개수) 몇개를 제거하고 , insertValue(넣을 값) 값을 몇개 넣어라
+console.log(movies)
+//5.hit_count(조회수)+1 한 객체를 반환
+return res.send(moviefind)
 })
- //영화등록
-app.post('/movies',(res, req) =>{
+
+
+             
+             
+//영화등록
+app.post('/movies', (req, res) =>{
    //1.사용자가 등록할 영화의 정보를 주면 받아옴 (from요청)(req)
    const newmovie = req.body
    //2.가져온 영화정보에 id 를 부여
-   newmovie.id = movies[movies.length - 1].id + 1
+   newmovie.id= movies[movies.length - 1].id + 1
    //3.조회수(hit_count) 는 기본으로 0으로 설정
    newmovie.hit_count = 0
    //4.작성일은 현재시간을 넣음
-   newmovie.created_at = new Date().toDateString()
+   newmovie.created_at = new Date(). toISOString()
    //5.(2,3,4번)전부 부여된 영화정보를 movies 추가
    movies.push(newmovie)
    console.log(movies)
-   res.send("영화등록")
+   res.send(newmovie)
 })
+
+
 
 
 /* //로그인
